def997621f4df6a379f7503e11523fb9b609f88f
YankeZhang <32335582+YankeZhang@users.noreply.github.com>
2019-04-12 14:50:30 +0100

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,26 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, SystemJsNgModuleLoaderConfig } from '@angular/core';
 import { NavController, ToastController} from 'ionic-angular';
 import { App } from 'ionic-angular';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { ReminderPage } from '../reminder/reminder'
+import { AngularFireDatabase } from 'angularfire2/database';
+import { database } from 'firebase';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(private fire:AngularFireAuth,
     public navCtrl: NavController,
     public app: App,
-    private toastController: ToastController) { }
+    private toastController: ToastController,
+    public firedb: AngularFireDatabase) { 
+      this.lastRecorded();
+    }
 
-  isRound: boolean = true;
-  isFull: boolean = true;
-  text: string = "Last Recorded time: "+ "19";
+  systolic;
+  diastolic;
+  glucose;
   
-  isAndroid: boolean = true;
-
   signOut()
   {
     const root = this.app.getRootNav();
@@ -29,7 +33,46 @@ export class HomePage {
   }
 
   goToReminderPage() {
-    this.navCtrl.push(ReminderPage);
-  }  
+    this.navCtrl.push('ReminderPage');
+  } 
+
+  goToContactPage() {
+    this.navCtrl.push("ContactDetailsPage");
+  }
+
+  lastRecorded() {
+    this.firedb.list("/users/"+this.fire.auth.currentUser.email.split('.').join('')+"/bloodpressure/record/systolic").valueChanges().subscribe(
+      _data =>
+      {
+        if(_data.length>0){
+          this.systolic="Systolic:\n"+_data[_data.length-1].toString();
+        }else{
+          this.systolic="Systolic:\n"+" no records";
+        }
+      }
+    );
+
+    this.firedb.list("/users/"+this.fire.auth.currentUser.email.split('.').join('')+"/bloodpressure/record/diastolic").valueChanges().subscribe(
+      _data =>
+      {
+        if(_data.length>0){
+          this.diastolic="Diastolic:\n"+_data[_data.length-1].toString();
+        }else{
+          this.diastolic="Diastolic:\n"+" no records";
+        }
+      }
+    );
+
+    this.firedb.list("/users/"+this.fire.auth.currentUser.email.split('.').join('')+"/bloodglucose/record").valueChanges().subscribe(
+      _data =>
+      {
+        if(_data.length>0){
+          this.glucose="Glucose:\n"+_data[_data.length-1].toString();
+        }else{
+          this.glucose="Glucose:\n"+" no records";
+        }
+      }
+    );
+  }
 
 }
